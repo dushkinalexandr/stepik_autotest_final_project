6858dfed190b4d8607d7c2e3a22152a16e0feb40
KU_22 <ku.wurm@google.com>
2021-03-07 15:07:51 +0300

    Modify metod login_pagein test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -11,31 +11,38 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
-def test_guest_should_see_login_url(browser):
+def test_guest_should_see_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page_form = LoginPage(browser, link)
     page_form.open()
-    page_form.should_be_login_url()
+    page_form.should_be_login_page()
 
 
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page_form = LoginPage(browser, link)
-    page_form.open()
-    page_form.should_be_login_form()
+# def test_guest_should_see_login_url(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page_form = LoginPage(browser, link)
+#     page_form.open()
+#     page_form.should_be_login_url()
+#
+#
+# def test_guest_should_see_login_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page_form = LoginPage(browser, link)
+#     page_form.open()
+#     page_form.should_be_login_form()
+#
+#
+# def test_guest_should_see_register_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page_form = LoginPage(browser, link)
+#     page_form.open()
+#     page_form.should_be_register_form()
 
-
-def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page_form = LoginPage(browser, link)
-    page_form.open()
-    page_form.should_be_register_form()
 
 
