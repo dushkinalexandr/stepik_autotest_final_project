c09cf781935236d402599e7b7d936abcb259bf5a
KU_22 <ku.wurm@google.com>
2021-03-08 22:40:21 +0300

    Add negative checks

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,10 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
+
 
 """
 Page Object, base page from which all other classes inherit.
@@ -28,6 +32,25 @@ class BasePage():
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        # упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        # будет ждать до тех пор, пока элемент не исчезнет
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
 
     def solve_quiz_and_get_code(self):
