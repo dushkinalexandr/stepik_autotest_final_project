5e450647fc8c68178316cc5d466afec89e0fb345
KU_22 <ku.wurm@google.com>
2021-03-25 21:37:43 +0300

    Modify tests basket_page checking

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -46,10 +47,11 @@ def test_guest_should_see_login_page(browser):
 #     page_form.open()
 #     page_form.should_be_register_form()
 
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
-    page = MainPage(browser, link)
+    page = BasketPage(browser, link)
     page.open()
-    page.got_to_basket_page()
+    page.go_to_basket_page()
     page.guest_cant_see_product_in_basket()
     page.guest_can_see_message_basket_is_empty()
