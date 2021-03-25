10e6f3ef65bb19e77883e5a2da6be3c6a006b111
KU_22 <ku.wurm@google.com>
2021-03-25 21:37:03 +0300

    Modify basket_page checking

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -10,5 +10,11 @@ Page Object, which is linked to the basket page.
 class BasketPage(BasePage):
     def guest_can_see_message_basket_is_empty(self):
         # Checking the display of the message
-        message_basket_empty = self.is_element_present(*BasketPageLocators.BASKET_EMPTY_MESSAGE)
-        assert message_basket_empty == "Your basket is empty.", "Basket not empty."
+        message = "Your basket is empty."
+        message_basket_empty = self.browser.find_element(*BasketPageLocators.BASKET_EMPTY_MESSAGE)
+        assert message in message_basket_empty.text, "Basket not empty."
+
+    def guest_cant_see_product_in_basket(self):
+        # Checking basket is empty
+        presence_product_in_basket = self.is_not_element_present(*BasketPageLocators.PRESENCE_PRODUCT_IN_BASKET)
+        assert presence_product_in_basket, "There are products in the basket"
