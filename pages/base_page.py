3fbdb8f6a4901d42027bd97ad9779cb31a7aee38
KU_22 <ku.wurm@google.com>
2021-03-25 21:44:21 +0300

    add language dict

--- pages/base_page.py
+++ pages/base_page.py
@@ -97,3 +97,31 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
+"""
+languages = {
+    "ar": "سلة التسوق فارغة",
+    "ca": "La seva cistella està buida.",
+    "cs": "Váš košík je prázdný.",
+    "da": "Din indkøbskurv er tom.",
+    "de": "Ihr Warenkorb ist leer.",
+    "en": "Your basket is empty.",
+    "el": "Το καλάθι σας είναι άδειο.",
+    "es": "Tu carrito esta vacío.",
+    "fi": "Korisi on tyhjä",
+    "fr": "Votre panier est vide.",
+    "it": "Il tuo carrello è vuoto.",
+    "ko": "장바구니가 비었습니다.",
+    "nl": "Je winkelmand is leeg",
+    "pl": "Twój koszyk jest pusty.",
+    "pt": "O carrinho está vazio.",
+    "pt-br": "Sua cesta está vazia.",
+    "ro": "Cosul tau este gol.",
+    "ru": "Ваша корзина пуста",
+    "sk": "Váš košík je prázdny",
+    "uk": "Ваш кошик пустий.",
+    "zh-cn": "Your basket is empty.",
+}
+
+language = self.browser.execute_script(
+            "return window.navigator.userLanguage || window.navigator.language")
+"""
