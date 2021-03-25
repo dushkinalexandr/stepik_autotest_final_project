cd15dfb702fcb2b880db88de16ddc5cfc456d816
KU_22 <ku.wurm@google.com>
2021-03-25 21:01:29 +0300

    Add checking basket is empty

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from ..locators import MainPageLocators
+from ..locators import BasketPageLocators
 
 # точка в импорте
 # ImportError: attempted relative import with no known parent package
@@ -12,3 +13,9 @@ Page Object, который связан с главной страницей и
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
+
+    def guest_can_see_message_basket_is_empty(self):
+        # Checking the display of the message
+        message = "Your basket is empty."
+        message_basket_empty = self.browser.find_element(*BasketPageLocators.BASKET_EMPTY_MESSAGE)
+        assert message in message_basket_empty.text, "Basket not empty."
