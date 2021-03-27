cbeae605da15f4e85a3ef62e1e8e0118580c6b2c
KU_22 <ku.wurm@google.com>
2021-03-27 12:09:52 +0300

    add class TestUserAddToBasketFromProductPage

--- test_product_page.py
+++ test_product_page.py
@@ -140,3 +140,19 @@ autouse=True - запускать автоматически без явного
 #     def test_guest_should_see_login_link(self, browser):
 #         page = ProductPage(browser, self.link)
 #         # дальше обычная реализация теста
+
+
+class TestUserAddToBasketFromProductPage():
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+
+    def test_user_cant_see_success_message(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.guest_can_add_product_to_basket()
+        # time.sleep(30)
+
